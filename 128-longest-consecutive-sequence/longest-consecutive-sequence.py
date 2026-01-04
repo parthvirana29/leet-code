04b77fcdd2e9676e2bf736b36531793944b2ce89
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-01-04 09:46:15 -0600

    Time: 45 ms (76.21%) | Memory: 34.4 MB (27.46%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -5,14 +5,16 @@ class Solution:
         # now thinking of a data structure to store it in a manner I can find the consecutive sequence
         # the only data structure with constant O(1) insertion would be hash map / sets
         # I didn't know how to move forward so I took a look at neetcode.io's hints section for this problem
-        # it suggested I create a set
+        # HARDEST PART: breaking the problem down into to steps: finding start, and using the start to find the sequence.
+        # it suggested I create a set so I created a set to get rid of duplicates
+        numsSet = set(nums)
         possibleStart = []
         longestSeq = 0
-        numsSet = set(nums)
+        # another hint it gave was to find the start of seq. A number can be considered start of seq if and only if the prev number does not exist.
         for i in numsSet:
             if i-1 not in numsSet:
                 possibleStart.append(i)
-        
+        # once I had possibleStart I iterate through them and calculate the consecutive sequence for each possible start. Store the longest sequence found and return it.
         for i in possibleStart:
             start = i
             while start + 1 in numsSet:
