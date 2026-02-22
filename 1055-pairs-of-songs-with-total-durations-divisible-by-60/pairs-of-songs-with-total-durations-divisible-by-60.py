3eec05a731b18e846abce05b7c15658bdba425e8
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-02-22 12:05:34 -0600

    Time: 24 ms (51.87%) | Memory: 23.4 MB (11.61%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,19 +1,17 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        # idxModMap = {30: [0,1,3]} value is a list with indices
-        idxModMap = {}
+        # freqModMap = {30: 3]}
+        freqModMap = {}
         CONST = 60
         result = 0
         for i in range(len(time)):
             mod = time[i] % CONST
             partnerMod = (CONST - mod) % 60
-            if partnerMod in idxModMap:
+            if partnerMod in freqModMap:
                 # do something:
-                result += len(idxModMap[partnerMod])
-            if (mod in idxModMap):
-                idxModMap[mod].append(i)
-            else:
-                idxModMap[mod] = [i]
+                result += freqModMap[partnerMod]
+            
+            freqModMap[mod] = freqModMap.get(mod,0) + 1
         return result
             
                 
