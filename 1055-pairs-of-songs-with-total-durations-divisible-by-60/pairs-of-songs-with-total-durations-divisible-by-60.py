984df0d715d543728d056c7661a9c67eac888bed
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-02-22 12:01:55 -0600

    Time: 39 ms (12.53%) | Memory: 24.5 MB (11.61%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,21 +1,19 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        # i tried using set previously but in that case I lost the occurences of each value. Hence creating frequency map
-        res = 0
-        freq = {}
+        # idxModMap = {30: [0,1,3]} value is a list with indices
+        idxModMap = {}
         CONST = 60
-        for i in time:
-            modI = i % 60
-            complement = abs(CONST - modI)%60
-            # storing remainders because any number with complement remainder will be a multiple of 60
-            if complement in freq:
-                res += freq[complement]
+        result = 0
+        for i in range(len(time)):
+            mod = time[i] % CONST
+            partnerMod = (CONST - mod) % 60
+            if partnerMod in idxModMap:
+                # do something:
+                result += len(idxModMap[partnerMod])
+            if (mod in idxModMap):
+                idxModMap[mod].append(i)
+            else:
+                idxModMap[mod] = [i]
+        return result
             
-            freq[modI] = freq.get(modI,0) + 1
-        return res
-            
-            
-            
-            
-
-        
+                
