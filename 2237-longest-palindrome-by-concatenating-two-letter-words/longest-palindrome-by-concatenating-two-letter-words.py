044c6454776a0477308f7e76d212ba0ad88d9af8
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-02-22 12:51:10 -0600

    Time: 331 ms (5.02%) | Memory: 36.3 MB (93.92%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,31 +1,26 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        # take the word if letters are same OR
-        # take the word if exact same pair exists
-        freq = {}
-        palindromeLen = 0
+        freqMap = {}
+        result = 0
+        seenDouble = False
         for i in words:
-            freq[i] = freq.get(i, 0) + 1
-        usedCenter = False
-        for key, value in freq.items():
-            opposite = key[1] + key[0]
-            if key[0] == key[1]:
-                pairs = value // 2
-                palindromeLen += pairs * 4
-                freq[key] -= pairs * 2
-                if (freq[key] > 0 and not usedCenter):
-                    freq[key] -= 1
-                    palindromeLen += 2
-                    usedCenter = True
+            reverse = i[1] + i[0]
+            print(reverse)
+            if reverse in freqMap:
+                if (freqMap[reverse] > 0):
+                    freqMap[reverse] -= 1
+                    result += 4
+                else:
+                    # you missed this important!! Add [i] to freqMap if not used
+                    freqMap[i] = freqMap.get(i,0) + 1
+            
+            else:
+                freqMap[i] = freqMap.get(i,0) + 1
+        for k,v in freqMap.items():
+            if k[0] == k[1] and v > 0:
+                result += 2
+                break
 
-                continue
+            
 
-            if opposite in freq:
-                minCount = min(value, freq[opposite])
-                palindromeLen += (min(value, freq[opposite])*4)
-                freq[opposite] -= minCount
-                freq[key] -= minCount
-            print("PalindromeLen: ", palindromeLen)
-        
-        return palindromeLen
-
+        return result
