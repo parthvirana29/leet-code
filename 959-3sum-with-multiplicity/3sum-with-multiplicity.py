743f0b5b59bb8b9f29240074ce5265ee0acc11dc
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-02-23 19:18:58 -0600

    Time: 8 ms (86.18%) | Memory: 19.8 MB (6.34%) - LeetSync

--- 959-3sum-with-multiplicity/3sum-with-multiplicity.py
+++ 959-3sum-with-multiplicity/3sum-with-multiplicity.py
@@ -1,31 +1,35 @@
+from collections import Counter
 class Solution:
     def threeSumMulti(self, arr: List[int], target: int) -> int:
-        arr.sort()
-        cnt = Counter(arr)  # obtain the number of instances of each number
-        res, i, l = 0, 0, len(arr)
+        MOD = 10**9 + 7
+        count = Counter(arr)
+        print(count)
+        keys = sorted(count)
+        print(keys)
+        result = 0
+        for i, num in enumerate(keys):
+            l,r = i, len(keys) - 1
+            complement = target - num
+            while (l <= r):
+                num2,num3 = keys[l], keys[r]
+                if (num2 + num3) < complement:
+                    l += 1
+                elif (num2 + num3) > complement:
+                    r -= 1
+                else:
+                    if i < l < r:
+                        result += count[num] * count[num2] * count[num3]
+                    # C(n, 2) = n! / (2! * (n-2)!) = n * (n-1) / 2
+                    elif i == l < r:
+                        result += (count[num] * (count[num2]-1))//2 * count[num3]
+                    elif i < l == r:
+                        result += count[num] * (count[num2] * (count[num3] -1))//2
+                    else:
+                        result += (count[num] * (count[num2] - 1) * (count[num3] - 2))//6
+                    l += 1
+                    r -= 1
+        return result % MOD
 
-        while i < l:  # in replacement of the for-loop, so that we can increment i by more than 1
-            j, k = i, l - 1  # j should be the leftmost index, hence j=i instead of j=i+1
 
-            while j < k:  # i <= j < k; arr[i] <= arr[j] <= arr[k]
-                if arr[i] + arr[j] + arr[k] < target:
-                    j += cnt[arr[j]]
-                elif arr[i] + arr[j] + arr[k] > target:
-                    k -= cnt[arr[k]]
 
-                else:  # arr[i] + arr[j] + arr[k] == target
-                    if arr[i] != arr[j] != arr[k]:  # Case 1: All the numbers are different
-                        res += cnt[arr[i]] * cnt[arr[j]] * cnt[arr[k]]
-                    elif arr[i] == arr[j] != arr[k]:  # Case 2: The smaller two numbers are the same
-                        res += cnt[arr[i]] * (cnt[arr[i]] - 1) * cnt[arr[k]] // 2  # math.comb(cnt[arr[i]], 2) * cnt[arr[k]]
-                    elif arr[i] != arr[j] == arr[k]:  # Case 3: The larger two numbers are the same
-                        res += cnt[arr[i]] * cnt[arr[j]] * (cnt[arr[j]] - 1) // 2  # math.comb(cnt[arr[j]], 2) * cnt[arr[i]]
-                    else:  # Case 4: All the numbers are the same
-                        res += cnt[arr[i]] * (cnt[arr[i]] - 1) * (cnt[arr[i]] - 2) // 6  # math.comb(cnt[arr[i]], 3)
-					# Shift pointers by the number of instances of the number
-                    j += cnt[arr[j]]
-                    k -= cnt[arr[k]]
 
-            i += cnt[arr[i]]  # Shift pointer by the number of instances of the number
-
-        return res % int(1e9 + 7)
