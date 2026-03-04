0efdbb7aba4b52389299cc4d371af16361788295
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-03 23:40:48 -0600

    Time: 3 ms (88.03%) | Memory: 19.6 MB (92.88%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -5,14 +5,11 @@ class Solution:
             if sum(mat[row]) == 1:
                 col = mat[row].index(1)
                 colSum = 0
-                print("col: ", col)
                 for i in range(len(mat)):
-                    print(mat[i][col])
-                    print(i,col)
+                  
                     if (mat[i][col]) == 1:
                         
                         colSum += 1
-                        print("ColSum: " , colSum)
                 if (colSum == 1):
                     res += 1
                 
