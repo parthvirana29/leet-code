701a014c2a0864a43227e55b97a9193a486519e4
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-06 23:57:56 -0600

    Time: 7 ms (83.13%) | Memory: 19.4 MB (24.30%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,26 +1,8 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        numChanges = 0
-        num2Changes = 1
-        arr = list(s)
-        arr2 = list(s)
-        arr2[0] ='1' if arr[0] == '0' else '0'
-        for i in range(1,len(s)):
-            if arr[i] == arr[i-1]:
-                if (arr[i-1] == '0'):
-                    arr[i] = '1'
-                else:
-                    arr[i] = '0'
-                numChanges += 1
-            if arr2[i] == arr2[i-1]:
-                if (arr2[i-1] == '0'):
-                    arr2[i] = '1'
-                else:
-                    arr2[i] = '0'
-                num2Changes += 1
-        return min(numChanges, num2Changes)
-
-    
-            
-
-            
+        count, n = 0, len(s)
+        for i in range(n):
+            expected = '0' if i % 2 == 0 else '1'
+            if s[i] != expected:
+                count += 1
+        return min(count, n - count)
