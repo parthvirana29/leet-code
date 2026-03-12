85e4a84404157b95a751663a912939e74984a956
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-11 23:08:56 -0500

    Time: 0 ms (100.00%) | Memory: 22.5 MB (82.60%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,7 +4,6 @@ class Solution:
         
         intervals.sort(key=lambda x: x[0])
         mergedIntervals = [intervals[0]]
-        print(intervals)
         for i in range(1,len(intervals)):
             if (mergedIntervals[-1][0] <= intervals[i][0] <= mergedIntervals[-1][1]):
                 mergedIntervals[-1][1] = max(intervals[i][1], mergedIntervals[-1][1])
