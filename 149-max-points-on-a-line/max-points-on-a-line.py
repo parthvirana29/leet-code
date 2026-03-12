f4b5d88a74b4d15f321f13bd34ba95f4ecbbf0d5
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-11 22:06:52 -0500

    Time: 15 ms (99.56%) | Memory: 19.3 MB (96.66%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.py
+++ 149-max-points-on-a-line/max-points-on-a-line.py
@@ -1,26 +1,30 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
+        # initialize the maxPointsOnLine with 1 because constraint mentions 1 <= points.length <= 300
         maxPointsOnLine = 1
-
+        # get the slope w.r.t each of the points
         for i in range(len(points)):
+            # generate fresh frequency for each slope w.r.t the curent point
             slopeFreq = {}
             x1, y1 = points[i][0], points[i][1]
+            # calculate the slope and update the slope freq
             for j in range(i+1,len(points)):
                 x2, y2 = points[j][0], points[j][1]
                 numerator = y2 - y1
                 denominator = x2-x1
+                # division by 0 is impossible
                 if (denominator == 0 ):
                     slope = float('inf')
                 else:
                     slope = numerator/denominator
 
-
+                # we are updating the slope freq with base value 1, because 2 points make up a slope
                 slopeFreq[slope] = slopeFreq.get(slope, 1) + 1
 
-            # find the maximum value
+            # find the maximum occurence for the slope w.r.t current point: points[i]
             for key, val in slopeFreq.items():
-                print(key, val)
                 maxPointsOnLine = max(maxPointsOnLine, val)
+        # return the maximum occurence of a slope
         return maxPointsOnLine
 
 
