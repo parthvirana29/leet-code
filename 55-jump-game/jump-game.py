1fb48c708d67eb4c8e742de04caaa60ea538a9b5
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-12 23:55:35 -0500

    Time: 27 ms (30.13%) | Memory: 20.5 MB (30.19%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,8 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         max_reach = 0
+        # for each idx check if you have reached it or you're behind it.
         for i in range(len(nums)):
-            if (i > max_reach):
+            # return false if your max reach is less than current idx. you're behind.
+            if (max_reach < i):
                 return False
+            # take the biggest step you can from a given index. 
             max_reach = max(max_reach, i + nums[i])
         return True
