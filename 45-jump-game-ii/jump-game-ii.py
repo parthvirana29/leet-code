07db7f9226ee2c82843ec5c79ceb771a2ba4a827
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-14 16:56:58 -0500

    Time: 139 ms (24.70%) | Memory: 20.2 MB (56.25%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -2,17 +2,19 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         if len(nums) <= 1:
             return 0
-        
         jumps = 0
         current_end = 0
         farthest = 0
-        
         for i in range(len(nums) - 1):
             farthest = max(farthest, i + nums[i])
-            
+            # in case we were not gauranteed to reach end
+            if farthest <= i:
+                return -1 
             if i == current_end:
                 jumps += 1
                 current_end = farthest
-                print("current_end: ", current_end, "farthest: ", farthest)
+
+            print("i: ", i, "nums[i]: ", nums[i], "current_end: ", current_end, "farthest: ", farthest, "jumps: ", jumps)
+                
         
         return jumps
