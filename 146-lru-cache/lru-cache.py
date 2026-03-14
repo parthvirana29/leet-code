dfac1813fe6baf44b65f7b0f916294fa3d64b8a8
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-14 16:33:25 -0500

    Time: 122 ms (58.74%) | Memory: 76.9 MB (79.52%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,19 +1,59 @@
-from collections import OrderedDict
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = OrderedDict()
+        self.dictionary = {}
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+    def add(self, node):
+        previous_end = self.tail.prev
+        previous_end.next = node
+        node.prev = previous_end
+        node.next = self.tail
+        self.tail.prev = node
+    
+    # we don't have to do any error checking because of extra (-1,-1) node
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
 
+    # we don't have to do any error checking because of extra (-1,-1) node
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.dictionary:
             return -1
-        self.cache.move_to_end(key)  # O(1)
-        return self.cache[key]
+
+        node = self.dictionary[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
+    
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)  # O(1)
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)  # O(1) evict LRU
+        if (key in self.dictionary):
+            # remove old node if one exists
+            old_node = self.dictionary[key]
+            self.remove(old_node)
+        # add a node with the key value towards the end and also keep track of it in the dictionary
+        node = ListNode(key,value)
+        self.add(node)
+        self.dictionary[key] = node
+        # check if we have gone past the capacity
+        if (len(self.dictionary) > self.capacity):
+            node_to_remove = self.head.next
+            del self.dictionary[node_to_remove.key]
+            self.remove(node_to_remove)
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
