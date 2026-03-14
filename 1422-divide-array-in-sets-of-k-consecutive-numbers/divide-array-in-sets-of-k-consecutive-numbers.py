129ed98980c6c9132020c1c8c859a9cf7e810c68
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-14 12:29:22 -0500

    Time: 113 ms (69.75%) | Memory: 37.5 MB (63.95%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        if (n % k != 0):
+            return False
+        freq = Counter(nums)
+        sorted_keys = sorted(freq.keys())
+        for num in sorted_keys :
+            if (freq[num] == 0):
+                continue
+            curr_freq = freq[num]
+            for i in range(k):
+                # ensure all k consecutive elements of num have the frequency >= freq[num]
+                if freq[num+i] < curr_freq:
+                    return False
+                # use up the element up to current_frequency
+                freq[num+i] -= curr_freq
+
+
+                
+        return True
+                           
