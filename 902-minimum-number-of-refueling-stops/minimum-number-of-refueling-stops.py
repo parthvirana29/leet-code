fe6bd1f8c57a8cfc8044ce73a50888a866480d55
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-14 22:07:34 -0500

    Time: 0 ms (100.00%) | Memory: 19.7 MB (31.07%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
@@ -1,27 +1,25 @@
 import heapq
-from typing import List
-
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        max_heap = []          # will store fuels as negative for max heap
-        fuel = startFuel
-        prev = 0
+        max_heap = []
         stops = 0
-        
-        stations.append([target, 0])   # handle final stretch
-        
-        for pos, cap in stations:
-            fuel -= (pos - prev)       # distance from last point
-            
-            # refuel when forced
-            while max_heap and fuel < 0:
-                fuel += -heapq.heappop(max_heap)
+        fuel = startFuel
+        prev = 0 # keeping track of previous to know how much distance i travelled from i-1 to i to see how much fuel i burned
+        # this ensures we reach the target and since we use prev, we consider all the edge cases.
+        stations.append([target,0])
+        for i in range(len(stations)):
+            pos, fuel_cap = stations[i][0], stations[i][1]
+            fuel -= (pos - prev)
+            while (max_heap and fuel < 0):
+                # pushing negative fuel_cap because heapq in Python implements min heap by default. Hence keeping - value so max fuel_cap will still be at top of heap
+                fuel +=  - heapq.heappop(max_heap)
                 stops += 1
-            
-            if fuel < 0:
+            if (fuel < 0):
+
                 return -1
-            
-            heapq.heappush(max_heap, -cap)
+            heapq.heappush(max_heap, -fuel_cap)
+
+
+
             prev = pos
-        
         return stops
