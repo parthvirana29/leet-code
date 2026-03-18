258a97f882d5fe4bc5f55ea943b76ac282d860df
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-18 14:59:08 -0500

    Time: 6 ms (27.73%) | Memory: 21.6 MB (28.65%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -2,19 +2,28 @@ from collections import deque
 import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals = sorted(intervals)
-        stack = [intervals[0][1]]
-        num_rooms = 1
-        for i in range(1,len(intervals)):
-            prev_end_time = stack[0]
-            curr_start_time = intervals[i][0]
-            # 3-30, 15-40, 32-35, 
-            if (prev_end_time > curr_start_time ):
-                num_rooms += 1
-            else:
-                heappop(stack)
+        start = []
+        end = []
+        for i in intervals:
+            start.append(i[0])
+            end.append(i[1])
+        start.sort()
+        end.sort()
+        print(start)
+        print(end)
+        s_pt = e_pt = 0
+        # 0, 5, 15
+        # 15, 20, 30
+        # num_rooms = 1
+        # 2, 7
+        # 4, 10
+        e_pt = 0
+        while (s_pt < len(intervals)):
+            if start[s_pt] >= end[e_pt]:
+                e_pt += 1
+            s_pt += 1
+        return s_pt - e_pt
+            
 
-            heapq.heappush(stack, intervals[i][1])
-        return num_rooms
-
+            
         
