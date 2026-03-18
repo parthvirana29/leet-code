9ead42b445233c45feaa14029cc585a938b586db
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-18 14:59:21 -0500

    Time: 3 ms (83.94%) | Memory: 21.3 MB (98.06%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -9,14 +9,8 @@ class Solution:
             end.append(i[1])
         start.sort()
         end.sort()
-        print(start)
-        print(end)
         s_pt = e_pt = 0
-        # 0, 5, 15
-        # 15, 20, 30
-        # num_rooms = 1
-        # 2, 7
-        # 4, 10
+
         e_pt = 0
         while (s_pt < len(intervals)):
             if start[s_pt] >= end[e_pt]:
