c19909de26f5195a6ac8b17b326e1055a5d6151b
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-19 12:51:18 -0500

    Time: 95 ms (24.93%) | Memory: 24.8 MB (19.16%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,36 +1,42 @@
 import random
-
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.pos_integers = w
+        # 3, 4, 1, 2
+        # P(3) = 3/10
+        # P(4) = 4/10 
+        # P(1) = 1/10
+        # P(2) = 2/10
+        # So range has to be sum of all the weights.
+        # Now I need to determine the inner range that is assigned to each number
+        # 3, *4* 7, 8, 10
+        # 1,2,3 = 3
+        # 4,5,6,7 = 4
+        # 8 = 1
+        # 9, 10 = 2
+
         self.prefix_sum = [w[0]]
-        # start at correct index range (1, len(arr)). 1 is important
-        for i in range(1,len(self.pos_integers)):
-            self.prefix_sum.append(self.prefix_sum[-1] + self.pos_integers[i])
-        
+        for i in range(1,len(w)):
+            self.prefix_sum.append(self.prefix_sum[i-1] + w[i])
+        # print(prefix_sum)
     def pickIndex(self) -> int:
-        start = 0
-        end = self.prefix_sum[-1]
-        random_num = random.uniform(start, end)
-        print(random_num)
+        val = random.uniform(0, self.prefix_sum[-1])
+        # find the val in prefix sum
         l, r = 0, len(self.prefix_sum)
         while (l < r):
-            # 3.5 
-            # 0, 1, 2, 3 
-            # 1, 3, 6, 10
-            mid = (l + r) // 2
-            if (self.prefix_sum[mid] < random_num):
-                # remember we don't move by l += 1 but l = mid + 1
-                l = mid + 1
-            elif self.prefix_sum[mid] > random_num:
-                # r does not equal r-= 1 but r = mid.
-                # this is why binary search is powerful each time it runs it cuts its area of iteration to half
+            mid = (l + r) //2
+            mid_val = self.prefix_sum[mid]
+            if (val < mid_val):
                 r = mid
-          
-        print("l: ", l, "r: ", r, "mid: ", mid )
+            elif (val > mid_val):
+                l = mid + 1
+            else:
+                return l
+  
         return l
-    
+            
+
+
         
 
 
