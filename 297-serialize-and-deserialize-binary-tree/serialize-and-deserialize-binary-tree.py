fdb191c657bdedde78994434bbce042c70a97411
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-28 17:01:25 -0400

    Time: 91 ms (47.71%) | Memory: 22.5 MB (80.52%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -1,32 +1,66 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
 class Codec:
+
     def serialize(self, root):
-        """Encodes a tree to a single string."""
+        """Encodes a tree to a single string.
         
-        def preorder(node, string):
-            if not node:
-                string += 'None,'
-            else:
-                string += str(node.val) + ','
-                string = preorder(node.left, string)
-                string = preorder(node.right, string)
-                # 1,2,None, None, 3, 4, None, None, 5, None, None
-            return string
-        return preorder(root,'')
-        
-        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ''
+
+        q = deque([root])
+        res = ''
+        while (q):
+            curr = q.popleft()
+            if not curr:
+                res += 'None,'
+                continue
+            res += str(curr.val) +','
+            q.append(curr.left)
+            q.append(curr.right)
+        print(res)
+        return res
+
     def deserialize(self, data):
-        """Decodes your encoded data to tree."""
-        def undo_preorder(lst):
-            if lst[0] == 'None':
-                lst.pop(0)
-                return None
-            root = TreeNode(lst[0])
-            lst.pop(0)
-            root.left = undo_preorder(lst)
-            root.right = undo_preorder(lst)
-            return root
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return None
+        lst = data.split(',')
+        root = TreeNode(lst[0])
+        q = deque([root])
+        i = 1
+        while (q and i < len(lst)):
+            curr = q.popleft()
+            if lst[i] != 'None':
+                left = TreeNode(int(lst[i]))
+                curr.left = left
+                q.append(left)
+            i += 1
+            if lst[i] != 'None':
+                right = TreeNode(int(lst[i]))
+                curr.right = right
+                q.append(right)
+            i += 1
+        return root
+
+            
 
         
-        data_list = data.split(',')
-        root = undo_preorder(data_list)
-        return root
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
