c4766dac0a812810d13e141dcc453a0e133ffbc4
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-29 00:43:06 -0400

    Time: 17 ms (86.47%) | Memory: 20.8 MB (77.41%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -2,26 +2,36 @@ from collections import Counter
 import heapq
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        freq = Counter(tasks)
-        heap =  [-f for f in list(freq.values())]
-        heapq.heapify(heap)
-        time = 0
-        while (heap):
-            cycle = n + 1
-            temp = []
-            tasks_done = 0
+        """
+        Example 1:
+        [A, A, A, B, B, B]
+        n = 2
         
-            while (cycle and heap):
-                freq = heapq.heappop(heap)
-                cycle -= 1
-                tasks_done += 1
-                if freq + 1 < 0:
-                    temp.append(freq+1)
-            
-            for t in temp:
-                heapq.heappush(heap, t)
-            time += n + 1 if heap else tasks_done
-        return time 
+        A _ _ A _ _ A
+        -> [A _ _] [A _ _] [A] -> (max_freq - 1)
+        => A _ _ => (n + 1)
+        ~> A & B both appear 3 times
+        (max_freq - 1) * (n + 1) + num_max = (3 - 1) * (2 + 1) + 2
 
+        Example 2:
+        [A, A, A, B, B]
+        n = 2
+        
+        (max_freq - 1) * (n + 1) + num_max = (3 - 1) * (2 + 1) + 1
 
+        Example 3:
+        [A, A, A, B, B, B, C, C, D, D]
+        n = 2
 
+        A _ _ A _ _ A _ _
+        A B _ A B _ A B _
+        A B C A B D A B C...
+        (max_freq - 1) * (n + 1) + num_max => not enough for all tasks
+
+        so, only len(tasks) will be enough (no idle needed)
+        """
+        task_count = Counter(tasks).values()
+        max_freq = max(task_count)
+        num_max = list(task_count).count(max_freq)
+
+        return max(len(tasks), (max_freq - 1) * (n + 1) + num_max)
