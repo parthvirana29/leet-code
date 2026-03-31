e566a4fcfe408e653823944ca5ad15f9119a0bb0
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-30 22:22:39 -0400

    Time: 141 ms (20.44%) | Memory: 25 MB (9.62%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -2,41 +2,25 @@ import random
 class Solution:
 
     def __init__(self, w: List[int]):
-        # 3, 4, 1, 2
-        # P(3) = 3/10
-        # P(4) = 4/10 
-        # P(1) = 1/10
-        # P(2) = 2/10
-        # So range has to be sum of all the weights.
-        # Now I need to determine the inner range that is assigned to each number
-        # 3, *4* 7, 8, 10
-        # 1,2,3 = 3
-        # 4,5,6,7 = 4
-        # 8 = 1
-        # 9, 10 = 2
-
-        self.prefix_sum = [w[0]]
+        self.arr = [w[0]]
         for i in range(1,len(w)):
-            self.prefix_sum.append(self.prefix_sum[i-1] + w[i])
-        # print(prefix_sum)
+            self.arr.append(w[i] + self.arr[i-1])
+        print(self.arr)
+
     def pickIndex(self) -> int:
-        val = random.uniform(0, self.prefix_sum[-1])
-        # find the val in prefix sum
-        l, r = 0, len(self.prefix_sum)
+        n = len(self.arr)
+        pick = random.uniform(0,self.arr[-1])
+        print(pick)
+        l, r = 0, n
         while (l < r):
             mid = (l + r) //2
-            mid_val = self.prefix_sum[mid]
-            if (val < mid_val):
-                r = mid
-            elif (val > mid_val):
+            if (pick > self.arr[mid]):
                 l = mid + 1
+            elif (pick < self.arr[mid]):
+                r = mid
             else:
                 return l
-  
         return l
-            
-
-
         
 
 
