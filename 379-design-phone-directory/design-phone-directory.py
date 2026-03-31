02848034f1c47bb68867c9ed5c4c1e7eb7b6b42d
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-30 20:53:54 -0400

    Time: 11 ms (56.95%) | Memory: 24 MB (25.11%) - LeetSync

--- 379-design-phone-directory/design-phone-directory.py
+++ 379-design-phone-directory/design-phone-directory.py
@@ -3,22 +3,16 @@ class PhoneDirectory:
     def __init__(self, maxNumbers: int):
         self.max_limit = maxNumbers
         self.available = set(range(maxNumbers))
-
     def get(self) -> int:
         if self.available:
             return self.available.pop()
         return -1
-    
-
     def check(self, number: int) -> bool:
         if number in self.available:
             return True
         return False
-        
 
     def release(self, number: int) -> None:
-        if number in self.available:
-            return
         self.available.add(number)
         
 
