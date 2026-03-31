abe268daa91e3dde317866025964e615207aeb98
parthvirana29 <102067795+parthvirana29@users.noreply.github.com>
2026-03-30 22:04:18 -0400

    Time: 4 ms (99.67%) | Memory: 20.4 MB (71.90%) - LeetSync

--- 1903-design-most-recently-used-queue/design-most-recently-used-queue.py
+++ 1903-design-most-recently-used-queue/design-most-recently-used-queue.py
@@ -1,20 +1,9 @@
 class MRUQueue:
 
     def __init__(self, n: int):
-        self.nums = SortedList((v,v) for v in range(1,n+1)) # pos, value
-        print(self.nums)
+        self.q = [i for i in range(1, n + 1)]
 
     def fetch(self, k: int) -> int:
-        # doing k-1 because list is 0 indexed
-        res = self.nums[k-1][1]
-        last_pos = self.nums[-1][0]
-        # delete current position
-        del self.nums[k-1]
-        # move it to end by updating the position value
-        self.nums.add((last_pos+1, res))
-        return res
-
-
-# Your MRUQueue object will be instantiated and called as such:
-# obj = MRUQueue(n)
-# param_1 = obj.fetch(k)
+        val = self.q.pop(k - 1)   # remove k-th element
+        self.q.append(val)        # move it to the end
+        return val
